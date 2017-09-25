461f903d0865e43b5fcef4c16f707af2113ebc14
Ed Ryan Alburo <edryanalburo@gmail.com>
2017-09-25 23:29:04 +0800

    initial commit

---
+++ src/app/todo-list-header/todo-list-header.component.ts
@@ -0,0 +1,27 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Todo} from "../todo";
+
+@Component({
+  selector: 'app-todo-list-header',
+  templateUrl: './todo-list-header.component.html',
+  styleUrls: ['./todo-list-header.component.css']
+})
+export class TodoListHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  newTodo: Todo = new Todo();
+
+  @Output()
+  add: EventEmitter<Todo> = new EventEmitter();
+
+  addTodo() {
+    this.add.emit(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+
+}
